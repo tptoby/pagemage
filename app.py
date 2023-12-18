00e8f60f73f75d61c65e27059ef0ea5aaa7f2d0a
tptoby <145922332+tptoby@users.noreply.github.com>
2023-12-17 17:08:14 -0800

    Update app.py
    change to pymongo

--- app.py
+++ app.py
@@ -6,44 +6,35 @@ Page Mage Panel App
 
 import panel as pn
 import pandas as pd
-import sqlite3
-import hvplot.pandas
+import pymongo
+
 
 
 """
 FUNCTIONS
 """
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    return conn
-
+def create_client():
+    client_str = "mongodb+srv://PageMageApp:3vc4u328TOrRBhmN@pagemage0.kwyg3z7.mongodb.net/"
+    client = pymongo.mongo_client.MongoClient(client_str, server_api=pymongo.server_api.ServerApi('1'))
+    return client
 
 def get_user_list (): 
-    conn = create_connection(db_path)
-    users = pd.read_sql_query("SELECT username FROM users;", conn)
-    conn.close()
+    cursor = collection_user.find()
+    users = pd.DataFrame(list(cursor))
     out = [i for i in users['username']]
     out.append("")
     return out
 
 def select_attribute (attribute):
-    conn = create_connection(db_path)
-    books = pd.read_sql_query('''SELECT "{}" FROM shelf;'''.format(attribute), conn)
-    conn.close()
+    cursor = collection_shelf.find({},{attribute: 1})
+    books = pd.DataFrame(list(cursor))
     out = [str(i) for i in books[attribute].unique()]
     out.append("")
     return out
 
 def get_book_scores (book): 
-    conn = create_connection(db_path)
-    df = pd.read_sql_query('''SELECT * FROM shelf;''', conn)
-    conn.close()
+    cursor = collection_shelf.find()
+    df = pd.DataFrame(list(cursor))
     df = df.loc[df['Unique ID'] == book, ].copy()
     scores = df[['User', 'CharacterDevelopment', 'Setting', 'Plot', 'Prose', 'MagicNovelty', 'RatingOverall', 'UnweightedRating']]
     scores = scores.set_index('User')
@@ -53,9 +44,8 @@ def get_book_scores (book):
     return scores
 
 def make_book_plot(book):
-    conn = create_connection(db_path)
-    df = pd.read_sql_query('''SELECT * FROM shelf;''', conn)
-    conn.close()
+    cursor = collection_shelf.find()
+    df = pd.DataFrame(list(cursor))
     df = df.loc[df['Unique ID'] == book, ].copy()
     scores = df[['User', 'CharacterDevelopment', 'Setting', 'Plot', 'Prose', 'MagicNovelty', 'RatingOverall', 'UnweightedRating']]
     scores = scores.set_index('User')
@@ -64,18 +54,16 @@ def make_book_plot(book):
     return scores.hvplot(kind='bar')
 
 def get_book_mtr (book): 
-    conn = create_connection(db_path)
-    df = pd.read_sql_query('''SELECT * FROM shelf;''', conn)
-    conn.close()
+    cursor = collection_shelf.find()
+    df = pd.DataFrame(list(cursor))
     df = df.loc[df['Unique ID'] == book, ].copy()
     mtr = df[['User', 'MtR1', 'MtR2', 'MtR3', 'MtR4']]
     mtr = mtr.set_index('User')
     return mtr
 
 def get_user_book (user, book): 
-    conn = create_connection(db_path)
-    df = pd.read_sql_query('''SELECT * FROM shelf;''', conn)
-    conn.close()
+    cursor = collection_shelf.find()
+    df = pd.DataFrame(list(cursor))
     df = df.loc[(df['Unique ID'] == book) & (df['User'] == user), ]
     return df
 
@@ -113,130 +101,98 @@ def calc_progress (CurrentPage, PageLength):
     return out
 
 
-def update_SQL(user, edit_title, edit_author, edit_series, edit_genre, edit_subgenre, edit_interest, edit_status,
+def update_DB(user, edit_title, edit_author, edit_series, edit_genre, edit_subgenre, edit_interest, edit_status,
                edit_synopsis, edit_mtr1, edit_mtr2, edit_mtr3, edit_mtr4, 
                edit_currentpage, edit_pagelength, edit_yearpublished, edit_priority, edit_chardev, edit_setting, edit_plot,
                edit_prose, edit_magnov, edit_unwtrate, event):
-    conn = create_connection(db_path)
-    cur = conn.cursor()
            
         #update book
-    sql = '''UPDATE shelf
-           SET 
-            Title = ? ,
-            Author = ? ,
-            Series = ?,
-            Genre = ?,
-            SubGenre = ?, 
-            Synopsis = ?, 
-            CurrentPage = ?, 
-            PageLength = ?, 
-            Progress = ?, 
-            YearPublished = ?, 
-            Status = ?, 
-            Interest = ?, 
-            Priority = ?, 
-            CharacterDevelopment = ?,
-            Setting = ?, 
-            Plot = ?, 
-            Prose = ?, 
-            MagicNovelty = ?, 
-            RatingOverall = ?, 
-            UnweightedRating = ?, 
-            MtR1 = ?, 
-            MtR2 = ?, 
-            MtR3 = ?, 
-            MtR4 = ? 
-          WHERE 
-            "Unique ID" = ? AND 
-            user = ?;
-          '''
-    #execution
-    data = (edit_title, 
-            edit_author,
-            edit_series,
-            edit_genre,
-            edit_subgenre,
-            edit_synopsis,
-            edit_currentpage,
-            edit_pagelength,
-            calc_progress(edit_currentpage, edit_pagelength),
-            edit_yearpublished,
-            edit_status,
-            edit_interest,
-            edit_priority,
-            edit_chardev,
-            edit_setting,
-            edit_plot,
-            edit_prose,
-            edit_magnov,
-            calc_overall_rating(edit_chardev, edit_setting, edit_plot, edit_prose, edit_magnov),
-            edit_unwtrate,
-            edit_mtr1,
-            edit_mtr2,
-            edit_mtr3,
-            edit_mtr4,
-            "{}_{}".format(edit_title, edit_author),
-            user,
-            )
-    
-    cur.execute(sql, data)
-    conn.commit()
-    conn.close()
-    mark_sql.object = "SQL Updated!"
+    collection_shelf.update_one(
+   { 
+      "Unique ID": f"{edit_title}_{edit_author}",
+      "User": user
+   },
+   {
+      "$set": {
+         "Title": edit_title,
+         "Author": edit_author,
+         "Series": edit_series,
+         "Genre": edit_genre,
+         "SubGenre": edit_subgenre,
+         "Synopsis": edit_synopsis,
+         "CurrentPage": edit_currentpage,
+         "PageLength": edit_pagelength,
+         "Progress": calc_progress(edit_currentpage, edit_pagelength),
+         "YearPublished": edit_yearpublished,
+         "Status": edit_status,
+         "Interest": edit_interest,
+         "Priority": edit_priority,
+         "CharacterDevelopment": edit_chardev,
+         "Setting": edit_setting,
+         "Plot": edit_plot,
+         "Prose": edit_prose,
+         "MagicNovelty": edit_magnov,
+         "RatingOverall": calc_overall_rating(edit_chardev, edit_setting, edit_plot, edit_prose, edit_magnov),
+         "UnweightedRating": edit_unwtrate,
+         "MtR1": edit_mtr1,
+         "MtR2": edit_mtr2,
+         "MtR3": edit_mtr3,
+         "MtR4": edit_mtr4
+             }
+          }
+       );
+
+    mark_sql.object = "Database Updated!"
     return None
 
-def insert_SQL(user_add, add_title, add_author, add_series, add_genre, add_subgenre, add_interest, add_status,
+def insert_DB(user_add, add_title, add_author, add_series, add_genre, add_subgenre, add_interest, add_status,
                add_synopsis, add_mtr1, add_mtr2, add_mtr3, add_mtr4, 
                add_currentpage, add_pagelength, add_yearpublished, add_priority, add_chardev, add_setting, add_plot,
                add_prose, add_magnov, add_unwtrate, event):
-    conn = create_connection(db_path)
-    cur = conn.cursor()
            
-    #insert book
-    sql = ''' INSERT INTO shelf(Title, Author, Series, Genre, SubGenre, Synopsis, CurrentPage, PageLength, 
-                                Progress, YearPublished, Status, Interest, Priority, CharacterDevelopment, 
-                                Setting, Plot, Prose, MagicNovelty, RatingOverall, UnweightedRating, 
-                                MtR1, MtR2, MtR3, MtR4, "Unique ID", user)
-          VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?); '''
-    #execution
-    data = (add_title, 
-            add_author,
-            add_series,
-            add_genre,
-            add_subgenre,
-            add_synopsis,
-            add_currentpage,
-            add_pagelength,
-            calc_progress(add_currentpage, add_pagelength),
-            add_yearpublished,
-            add_status,
-            add_interest,
-            add_priority,
-            add_chardev,
-            add_setting,
-            add_plot,
-            add_prose,
-            add_magnov,
-            calc_overall_rating(add_chardev, add_setting, add_plot, add_prose, add_magnov),
-            add_unwtrate,
-            add_mtr1,
-            add_mtr2,
-            add_mtr3,
-            add_mtr4,
-            "{}_{}".format(add_title, add_author),
-            user_add,
-            )
-    
-    cur.execute(sql, data)
-    conn.commit()
-    conn.close()
-    mark_sql_add.object = "SQL Updated!"
+        #update book
+    collection_shelf.insert_one(
+   {
+         "Title": add_title,
+         "Author": add_author,
+         "Series": add_series,
+         "Genre": add_genre,
+         "SubGenre": add_subgenre,
+         "Synopsis": add_synopsis,
+         "CurrentPage": add_currentpage,
+         "PageLength": add_pagelength,
+         "Progress": calc_progress(add_currentpage, add_pagelength),
+         "YearPublished": add_yearpublished,
+         "Status": add_status,
+         "Interest": add_interest,
+         "Priority": add_priority,
+         "CharacterDevelopment": add_chardev,
+         "Setting": add_setting,
+         "Plot": add_plot,
+         "Prose": add_prose,
+         "MagicNovelty": add_magnov,
+         "RatingOverall": calc_overall_rating(add_chardev, add_setting, add_plot, add_prose, add_magnov),
+         "UnweightedRating": add_unwtrate,
+         "MtR1": add_mtr1,
+         "MtR2": add_mtr2,
+         "MtR3": add_mtr3,
+         "MtR4": add_mtr4, 
+         "User": user_add,
+         "Unique ID": f"{add_title}_{add_author}"
+             }
+       );
+
+    mark_sql_add.object = "Database Updated!"
     return None
+
 """
-DECLARED VARIABLES
-"""  
-db_path = "DB_page_mage.db"
+ESTABLISH DB CONNECTIONS
+"""
+client = create_client()
+db = client['pagemage']
+collection_user = db['users']
+collection_shelf = db['shelf']
+
 
 """
 PANEL APP
@@ -244,9 +200,9 @@ PANEL APP
 
 #CURRENT BOOK
 
-select_book = pn.widgets.AutocompleteInput(name='Select Book', options=select_attribute("Unique ID"), placeholder='Title_Author Key', restrict=True)
+select_book = pn.widgets.AutocompleteInput(name='Select Book', options=select_attribute( "Unique ID"), placeholder='Title_Author Key', restrict=True)
 
-bind_scores = pn.bind(get_book_scores, select_book)
+bind_scores = pn.bind(get_book_scores, book = select_book)
 disp_scores = pn.pane.DataFrame(bind_scores)
 
 bind_mtr = pn.bind(get_book_mtr, select_book)
@@ -258,7 +214,7 @@ disp_plot = pn.panel(bind_plot, sizing_mode='stretch_width')
 
 #EDIT BOOK
 user = pn.widgets.AutocompleteInput(name='Select User', options = get_user_list(), placeholder="", restrict=True)
-user_book = pn.widgets.AutocompleteInput(name='Select Book', options=select_attribute("Unique ID"), placeholder='Title_Author Key', restrict=True)
+user_book = pn.widgets.AutocompleteInput(name='Select Book', options=select_attribute( "Unique ID"), placeholder='Title_Author Key', restrict=True)
 
 bind_user_book = pn.bind(get_user_book, user=user, book=user_book)
 
@@ -267,13 +223,13 @@ disp_user_book = pn.pane.DataFrame(bind_user_book, index=False)
 
 
 #Inputs with Value = selected book
-edit_title = pn.widgets.AutocompleteInput(name='Edit Title', options=select_attribute("Title"), value = pn.bind(get_value, df = bind_user_book, attribute="Title", dtype = str))
-edit_author = pn.widgets.AutocompleteInput(name='Edit Author (Last, First)', options=select_attribute("Author"), value = pn.bind(get_value, df = bind_user_book, attribute="Author", dtype = str))
-edit_series = pn.widgets.AutocompleteInput(name='Edit Series', options=select_attribute("Series"), value = pn.bind(get_value, df = bind_user_book, attribute="Series", dtype = str))
-edit_genre = pn.widgets.AutocompleteInput(name='Edit Genre', options=select_attribute("Genre"), value = pn.bind(get_value, df = bind_user_book, attribute="Genre", dtype = str))
-edit_subgenre = pn.widgets.AutocompleteInput(name='Edit SubGenre', options=select_attribute("SubGenre"), value = pn.bind(get_value, df = bind_user_book, attribute="SubGenre", dtype = str))
-edit_interest = pn.widgets.AutocompleteInput(name='Edit Interest', options=select_attribute("Interest"), value = pn.bind(get_value, df = bind_user_book, attribute="Interest", dtype = str))
-edit_status = pn.widgets.AutocompleteInput(name='Edit Status', options=select_attribute("Status"), value = pn.bind(get_value, df = bind_user_book, attribute="Status", dtype = str))
+edit_title = pn.widgets.AutocompleteInput(name='Edit Title', options=select_attribute( "Title"), value = pn.bind(get_value, df = bind_user_book, attribute="Title", dtype = str))
+edit_author = pn.widgets.AutocompleteInput(name='Edit Author (Last, First)', options=select_attribute( "Author"), value = pn.bind(get_value, df = bind_user_book, attribute="Author", dtype = str))
+edit_series = pn.widgets.AutocompleteInput(name='Edit Series', options=select_attribute( "Series"), value = pn.bind(get_value, df = bind_user_book, attribute="Series", dtype = str))
+edit_genre = pn.widgets.AutocompleteInput(name='Edit Genre', options=select_attribute( "Genre"), value = pn.bind(get_value, df = bind_user_book, attribute="Genre", dtype = str))
+edit_subgenre = pn.widgets.AutocompleteInput(name='Edit SubGenre', options=select_attribute( "SubGenre"), value = pn.bind(get_value, df = bind_user_book, attribute="SubGenre", dtype = str))
+edit_interest = pn.widgets.AutocompleteInput(name='Edit Interest', options=select_attribute( "Interest"), value = pn.bind(get_value, df = bind_user_book, attribute="Interest", dtype = str))
+edit_status = pn.widgets.AutocompleteInput(name='Edit Status', options=select_attribute( "Status"), value = pn.bind(get_value, df = bind_user_book, attribute="Status", dtype = str))
 
 
 edit_synopsis = pn.widgets.TextInput(name='Edit Synopsis', value = pn.bind(get_value, df = bind_user_book, attribute="Synopsis", dtype = str))
@@ -295,7 +251,7 @@ edit_magnov = pn.widgets.FloatInput(name='Edit Magiv / Novelty (1=low to 10=high
 edit_unwtrate = pn.widgets.FloatInput(name='Edit Unweighted Rating (1=low to 10=high)', value = pn.bind(get_value, df = bind_user_book, attribute="UnweightedRating", dtype = float), start=1, end=10)
 
 
-bind_edit_book = pn.bind(update_SQL, user, edit_title, edit_author, edit_series, edit_genre, edit_subgenre, edit_interest, edit_status,
+bind_edit_book = pn.bind(update_DB, user, edit_title, edit_author, edit_series, edit_genre, edit_subgenre, edit_interest, edit_status,
                edit_synopsis, edit_mtr1, edit_mtr2, edit_mtr3, edit_mtr4, 
                edit_currentpage, edit_pagelength, edit_yearpublished, edit_priority, edit_chardev, edit_setting, edit_plot,
                edit_prose, edit_magnov, edit_unwtrate) 
@@ -308,13 +264,13 @@ button_sql.on_click(bind_edit_book)
 user_add = pn.widgets.AutocompleteInput(name='Select User', options = get_user_list(), placeholder="", restrict=True)
 
 #Inputs 
-add_title = pn.widgets.AutocompleteInput(name='Edit Title', options=select_attribute("Title"), restrict=False)
-add_author = pn.widgets.AutocompleteInput(name='Edit Author (Last, First)', options=select_attribute("Author"), restrict=False)
-add_series = pn.widgets.AutocompleteInput(name='Edit Series', options=select_attribute("Series"), restrict=False)
-add_genre = pn.widgets.AutocompleteInput(name='Edit Genre', options=select_attribute("Genre"), restrict=False)
-add_subgenre = pn.widgets.AutocompleteInput(name='Edit SubGenre', options=select_attribute("SubGenre"), restrict=False)
-add_interest = pn.widgets.AutocompleteInput(name='Edit Interest', options=select_attribute("Interest"), restrict=False)
-add_status = pn.widgets.AutocompleteInput(name='Edit Status', options=select_attribute("Status"), restrict=False)
+add_title = pn.widgets.AutocompleteInput(name='Edit Title', options=select_attribute( "Title"), restrict=False)
+add_author = pn.widgets.AutocompleteInput(name='Edit Author (Last, First)', options=select_attribute( "Author"), restrict=False)
+add_series = pn.widgets.AutocompleteInput(name='Edit Series', options=select_attribute( "Series"), restrict=False)
+add_genre = pn.widgets.AutocompleteInput(name='Edit Genre', options=select_attribute( "Genre"), restrict=False)
+add_subgenre = pn.widgets.AutocompleteInput(name='Edit SubGenre', options=select_attribute( "SubGenre"), restrict=False)
+add_interest = pn.widgets.AutocompleteInput(name='Edit Interest', options=select_attribute( "Interest"), restrict=False)
+add_status = pn.widgets.AutocompleteInput(name='Edit Status', options=select_attribute( "Status"), restrict=False)
 
 
 add_synopsis = pn.widgets.TextInput(name='Edit Synopsis')
@@ -336,7 +292,7 @@ add_magnov = pn.widgets.FloatInput(name='Edit Magiv / Novelty (1=low to 10=high)
 add_unwtrate = pn.widgets.FloatInput(name='Edit Unweighted Rating (1=low to 10=high)', start=1, end=10)
 
 
-bind_add_book = pn.bind(insert_SQL, user_add, add_title, add_author, add_series, add_genre, add_subgenre, add_interest, add_status,
+bind_add_book = pn.bind(insert_DB, user_add, add_title, add_author, add_series, add_genre, add_subgenre, add_interest, add_status,
                add_synopsis, add_mtr1, add_mtr2, add_mtr3, add_mtr4, 
                add_currentpage, add_pagelength, add_yearpublished, add_priority, add_chardev, add_setting, add_plot,
                add_prose, add_magnov, add_unwtrate) 
